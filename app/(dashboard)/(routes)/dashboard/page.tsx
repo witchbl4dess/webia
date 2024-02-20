420fc236cfb3b2a836ff6e98e9f707bcf5b0f4fd
menzo <menzo.smit-adam@epitech.eu>
2024-02-20 21:30:11 +0100

    card style

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,75 @@
+"use client"
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { ArrowRight, Code, Image, MessageSquare, Music, Video } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generation",
+    icon: Image,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: Video,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: "/code",
+  },
+]
+
 const DashboardPage = () => {
   return (
     <div>
-      <p>DashboardPage (Protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font bold text-center">
+          Explore the power of IA
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smaterst AI in the world and get the best answers to your questions.
+        </p>
+      </div>
+      <div className="px- 4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card key={tool.href}
+          className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
+              <div className="font-semibold">
+                {tool.label}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5"/>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
--- components/basic/responsive-sidebar.tsx
+++ components/basic/responsive-sidebar.tsx
@@ -4,8 +4,16 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/basic/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+    if(!isMounted) {
+        return null;
+    }
     return (
         <Sheet>
             <SheetTrigger>
